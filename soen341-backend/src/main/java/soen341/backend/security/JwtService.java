9c02363a0477ba03c02cec753299af6f04a0dad9
DaKingzz <artsanes68@gmail.com>
2025-02-10 16:32:44 -0500

    Authentication is fully functional, one can register/verify code/login successfully.

--- soen341-backend/src/main/java/soen341/backend/security/JwtService.java
+++ soen341-backend/src/main/java/soen341/backend/security/JwtService.java
@@ -16,7 +16,7 @@ import java.util.Map;
 import java.util.function.Function;
 
 @Service
-public class Jwtservice {
+public class JwtService {
 
     @Value("${application.security.jwt.secret.expiration}")
     private long jwtExpiration;
@@ -27,7 +27,7 @@ public class Jwtservice {
         return generateToken(new HashMap<>(), userDetails);
     }
 
-    private String generateToken(Map<String, Object> claims, UserDetails userDetails){
+    public String generateToken(Map<String, Object> claims, UserDetails userDetails){
         return buildToken(claims, userDetails, jwtExpiration);
     }
 
