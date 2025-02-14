ebb8f7af6d956d93692097d13c28f88cb5e70abc
robert-louis-lando <robertlouis_lando@outlook.com>
2025-02-14 13:30:44 -0500

    websocket(connection):frontend-completed

--- soen341-frontend/src/app/direct-messages/direct-messages.component.ts
+++ soen341-frontend/src/app/direct-messages/direct-messages.component.ts
@@ -1,11 +1,71 @@
-import { Component } from '@angular/core';
-
+import { Component, OnInit } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+import { Client, Message } from '@stomp/stompjs';
+import { FormsModule } from '@angular/forms';
+import { Subscription } from 'rxjs';
 @Component({
   selector: 'app-direct-messages',
-  imports: [],
+  imports: [FormsModule],
   templateUrl: './direct-messages.component.html',
-  styleUrl: './direct-messages.component.css'
+  styleUrl: './direct-messages.component.css',
 })
-export class DirectMessagesComponent {
+export class DirectMessagesComponent implements OnInit {
+  private client: Client;
+  messages: string[] = [];
+  username: string = '1';
+  isConnected: boolean = false;
+  singleMessage: string = '';
+  // @ts-ignore, to suppress warning related to being undefined
+  private topicSubscription: Subscription;
+  constructor() {
+    this.client = new Client({
+      brokerURL: 'ws://localhost:8080/gs-guide-websocket',
+    });
+    this.client.onConnect = (frame) => {
+      console.log('Connected: ' + frame);
+      this.subcribe();
+    };
+  }
+  ngOnInit(): void {}
+  setAlice() {
+    this.username = '1';
+    console.log('setting to alice');
+  }
 
+  setBob() {
+    this.username = '2';
+    console.log('setting to bob');
+  }
+  connect() {
+    this.client.activate();
+    this.isConnected = true;
+    console.log('Connected');
+  }
+  disconnect() {
+    this.client.deactivate();
+    this.isConnected = false;
+    console.log('Disconnected');
+  }
+  sendName() {
+    console.log('trying to send: ' + this.singleMessage);
+    if (this.isConnected) {
+      this.client.publish({
+        destination: `/app/hello/${this.username}`,
+        body: JSON.stringify({ name: this.singleMessage }),
+      });
+    }
+  }
+
+  showGreeting(message: string) {
+    this.messages.push(message);
+  }
+
+  subcribe() {
+    this.client.subscribe(`/topic/greetings/${this.username}`, (greeting) => {
+      this.showGreeting(JSON.parse(greeting.body).content);
+    });
+  }
+  ngOnDestroy() {
+    this.topicSubscription.unsubscribe();
+  }
 }
