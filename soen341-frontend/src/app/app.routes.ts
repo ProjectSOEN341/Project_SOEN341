7b5525909dde678bc4897f7decb4a553b2fa461f
robert-louis-lando <robertlouis_lando@outlook.com>
2025-02-14 10:27:47 -0500

    setting-up(routes): websocket-component

--- soen341-frontend/src/app/app.routes.ts
+++ soen341-frontend/src/app/app.routes.ts
@@ -1,3 +1,5 @@
 import { Routes } from '@angular/router';
+import { DirectMessagesComponent } from './direct-messages/direct-messages.component';
+import { AppComponent } from './app.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [{path: '', component: AppComponent},{path: 'testing/dm', component: DirectMessagesComponent}];
