d95338635b82e49c93daa42fe77b24714b6d2771
Robert Louis Lando <robertlouis_lando@outlook.com>
2025-02-26 14:46:24 -0500

    reworked-request-path-websockets

--- soen341-frontend/src/app/direct-messages/direct-messages.component.ts
+++ soen341-frontend/src/app/direct-messages/direct-messages.component.ts
@@ -22,7 +22,7 @@ export class DirectMessagesComponent {
 
   constructor() {
     this.client = new Client({
-      brokerURL: 'ws://localhost:8080/gs-guide-websocket',
+      brokerURL: 'ws://localhost:8088/api/v1/dm/gs-guide-websocket', 
     });
     this.client.onConnect = (frame) => {
       console.log('Connected: ' + frame);
