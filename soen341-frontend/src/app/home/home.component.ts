a56d2cb607ca5cfe70178137071d8e762490b778
Robert Louis Lando <robertlouis_lando@outlook.com>
2025-02-26 18:48:05 -0500

    functional-messaging-connected-to-the-frontend

--- soen341-frontend/src/app/home/home.component.ts
+++ soen341-frontend/src/app/home/home.component.ts
@@ -5,7 +5,7 @@ import { Client } from '@stomp/stompjs';
 import { UserService } from '../services/userService.service';
 import { FormsModule } from '@angular/forms';
 import { Message } from '../../interfaces/message';
-import { HttpClient } from '@angular/common/http';
+import { HttpClient, HttpClientModule } from '@angular/common/http';
 import { Conversation } from '../../interfaces/conversation';
 
 @Component({
@@ -18,12 +18,13 @@ import { Conversation } from '../../interfaces/conversation';
 export class HomeComponent {
   private client: Client;
   url='http://localhost:8088/api/v1'
-  messages: string[] = [];
+  messages: Message[] = [];
   singleMessage: string = '';
   message!: Message;
   newConversationReceiver='';
+  selectedConversation!:Conversation;
    constructor(
-    private userService:UserService,
+    public userService:UserService,
     private http: HttpClient
    ) {
       this.client = new Client({
@@ -34,6 +35,7 @@ export class HomeComponent {
         this.subcribe();
       };
       this.client.activate();
+      this.http.get<Conversation[]>('http://localhost:8088/api/v1/direct-message/'+this.userService.loginUser.email).subscribe((conversations)=>this.conversations=conversations);
     }
 
   view: 'channels' | 'dms' = 'channels';
@@ -46,38 +48,54 @@ export class HomeComponent {
     { name: 'Thomas', messages: [{ user: 'Thomas', text: 'Hey, how are you?' }] },
     { name: 'Robert', messages: [{ user: 'Robert', text: "What's up?" }] },
   ];
-  conversations=["hello","swikjndf","wjeuhfn"]
+  conversations:Conversation[]=[];
 
   selectedChannel = this.channels[0];
   newMessage = '';
 
   subcribe() {
-    this.client.subscribe(`/dm/topic/greetings/${this.userService.loginUser.email}`, (m) => {
-      this.showGreeting(JSON.parse(m.body).body);
+    this.client.subscribe(`/topic/greetings/${this.userService.loginUser.email}`, (m) => {
+      this.showGreeting(JSON.parse(m.body));
     });
   }
   sendName() {
     
     console.log('trying to send: ' + this.singleMessage);
-    
+      const receiver=this.selectedConversation.user1==this.userService.loginUser.email?this.selectedConversation.user2:this.selectedConversation.user1;
       this.message = {
         sender: this.userService.loginUser.email,
-        receiver: "johnny",
+        receiver: receiver,
         body: this.singleMessage,
         timestamp: new Date().toLocaleString(),
       }
       this.client.publish({
-        destination: `/app/hello/${"johnny"}`,
+        destination: `/app/hello/${receiver}`,
         body: JSON.stringify(this.message),
       });
+      this.selectedConversation.messages.push(this.message);
     
   }
-  showGreeting(message: string) {
-    this.messages.push(message);
+  showGreeting(message: Message) {
+    const otherPerson=this.selectedConversation.user1==this.userService.loginUser.email?this.selectedConversation.user2:this.selectedConversation.user1;
+    if(message.sender==otherPerson){
+      this.selectedConversation.messages.push(message);
+    }
+    else{
+      this.conversations.forEach((convo)=>{
+        const convoPerson=convo.user1==this.userService.loginUser.email?convo.user2:convo.user1;
+        if(convoPerson==message.sender){
+          convo.messages.push(message);
+        }
+      })
+
+      }
+    }
     
-  }
+    
+  
   selectChannel(channel: any) {
     this.selectedChannel = channel;
+    
   }
   createConversation():void{
     const newConversation:Conversation = {
@@ -89,9 +107,24 @@ export class HomeComponent {
     };
     
     
-      this.http.post('http://localhost:8088/api/v1/direct-message/dm/createConversation', newConversation, {
-      })
+    this.http.post('http://localhost:8088/api/v1/direct-message/dm/createConversation', newConversation, {
+      headers: { 'Content-Type': 'application/json' }
+    })
+    .subscribe(response => {
+      console.log('Conversation created successfully', response);
+    }, error => {
+      console.error('Error creating conversation', error);
+    });
+    setTimeout(() => {
+      this.http.get<Conversation[]>('http://localhost:8088/api/v1/direct-message/'+this.userService.loginUser.email).subscribe((conversations)=>this.conversations=conversations);
+  }, 2000);
+  
+    
 
+  }
+  selectConversation(selectedConversation:Conversation):void{
+    this.selectedConversation=selectedConversation;
+    
   }
 
   sendMessage() {
